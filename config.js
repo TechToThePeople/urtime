14f954e654e9bef8ca70915181b522ef827d5b1b
Xavier Dutoit <xavier@tttp.eu>
2012-11-11 21:01:17 +0100

    trying to workaround the workaround (ad eternam)

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 if (process.env.NODE_ENV ==='production') {
   exports = {
-    user: { 
+    nokuser: { 
         user:process.env.XMPP_USER,
         domain:process.env.XMPP_DOMAIN
     },
+    user:process.env.XMPP_USER,
     password: process.env.XMPP_PASSWORD,
     server: 'talk.google.com',
     port: '5222' 
   };
-console.log(process.env);
 console.log(exports);
 } else {
   var cjson = require('cjson');
