bd55ca00bd8bd834b353d8fd14fe20d700404430
Andreas Hennings <andreas@dqxtech.net>
2012-11-11 22:20:25 +0100

    invalid.js to show help after 600 millis

--- commands/invalid.js
+++ commands/invalid.js
@@ -1,6 +1,13 @@
-function run(msg, user) {
-  return user.firstname + "... I'm afraid I can't let you do that." +
-    " \nInvalid command '" + msg + "'"
+var commandsJs = require('../lib/commands.js')
+
+
+function run(msg, user, say) {
+  setTimeout(function(){
+    say('message', commandsJs.getCommandsHelp())
+  }, 600)
+  say('message', user.firstname + "... I'm afraid I can't let you do that." +
+    " \nInvalid command '" + msg + "'")
 }
+
 
 module.exports = function(bot){return {name: 'invalid', run: run}}
