face57781a08ddd439dedfbbe8069320cf4fd9f2
Xavier Dutoit <xavier@tttp.eu>
2012-11-11 20:16:43 +0100

    modify so config works on prod (hopefully)

--- config.js
+++ config.js
@@ -1,5 +1,15 @@
-var cjson = require('cjson');
-var conf = cjson.load('./conf/config.json');
-for (var k in conf) {
-  exports[k] = conf[k];
-}
+if (process.env.NODE_ENV ==='production') {
+  exports = {
+    user: process.env.XMPP_USER,
+    password: process.env.XMPP_PASSWORD,
+    server: 'talk.google.com',
+    port: '5222' 
+  };
+} else {
+  var cjson = require('cjson');
+  var conf = cjson.load('./conf/config.json');
+  for (var k in conf) {
+    exports[k] = conf[k];
+  }
+};
+
